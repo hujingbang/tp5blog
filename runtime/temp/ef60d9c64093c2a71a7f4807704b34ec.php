3122445661c0dd4687ddb9ea8a44fc77f4941759
hjb <2292756556@qq.com>
2018-01-25 14:06:41 +0800

    blog项目

---
+++ runtime/temp/ef60d9c64093c2a71a7f4807704b34ec.php
@@ -0,0 +1,14 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:63:"F:\thinkphp5\public/../application/admin\view\public\error.html";i:1516676488;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1><?php echo $data; ?></h1>
+<h1><?php echo $msg; ?></h1>
+<h1><?php echo $code; ?></h1>
+<p><?php echo $wait; ?></p>
+<p><?php echo $url; ?></p>
+</body>
+</html>
